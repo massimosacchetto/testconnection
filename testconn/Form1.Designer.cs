4053396e2c5904e6b76ddbad8ce0e921793dbc69
Massimo Sacchetto <massimo.sacchetto@live.com>
2020-12-07 00:04:50 +0100

    Aggiunto grafici

--- testconn/Form1.Designer.cs
+++ testconn/Form1.Designer.cs
@@ -30,6 +30,12 @@ namespace testconn
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.IP1 = new System.Windows.Forms.TextBox();
             this.IP2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
@@ -37,6 +43,14 @@ namespace testconn
             this.output_a = new System.Windows.Forms.ListBox();
             this.output_b = new System.Windows.Forms.ListBox();
             this.timerPing = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
             // IP1
@@ -45,6 +59,7 @@ namespace testconn
             this.IP1.Name = "IP1";
             this.IP1.Size = new System.Drawing.Size(158, 22);
             this.IP1.TabIndex = 0;
+            this.IP1.Text = "8.8.8.8";
             // 
             // IP2
             // 
@@ -98,19 +113,95 @@ namespace testconn
             this.timerPing.Interval = 1000;
             this.timerPing.Tick += new System.EventHandler(this.timerPing_Tick);
             // 
+            // chart1
+            // 
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(447, 45);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(452, 159);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "IP Address 2";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(35, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "IP Address 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(199, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "IP Address 2";
+            // 
+            // chart2
+            // 
+            this.chart2.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(447, 228);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(452, 159);
+            this.chart2.TabIndex = 9;
+            this.chart2.Text = "IP Address 2";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(444, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "IP Address 2";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(444, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 18);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "IP Address 1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(911, 407);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.output_b);
             this.Controls.Add(this.output_a);
             this.Controls.Add(this.stop_ping);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.IP2);
             this.Controls.Add(this.IP1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "testconnection";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,6 +216,12 @@ namespace testconn
         private System.Windows.Forms.ListBox output_a;
         private System.Windows.Forms.ListBox output_b;
         private System.Windows.Forms.Timer timerPing;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- testconn/Form1.cs
+++ testconn/Form1.cs
@@ -8,12 +8,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.NetworkInformation;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace testconn
 {
     public partial class Form1 : Form
         
     {
+        const int PINGVETT = 20; 
+        //public int pingDeth = 20;
+        public int[] chartMainSeries = new int[PINGVETT];
+        public int[] chartSeries1 = new int[PINGVETT];
+        public int[] chartSeries2 = new int[PINGVETT];
+        public int pingDeth = PINGVETT;
+
         public Form1()
         {
             InitializeComponent();
@@ -76,18 +84,86 @@ namespace testconn
 
         private void timerPing_Tick(object sender, EventArgs e)
         {
+            
             string host_a = Convert.ToString(this.IP1.Text);
             string host_b = Convert.ToString(this.IP2.Text);
+            //ping 1
             if (!string.IsNullOrEmpty(host_a))
             {
-                Console.Out.WriteLine(hostping(host_a));
-                scrivioutput_a(host_a + " - " + Convert.ToString(hostping(host_a)));
+                int maxvett = pingDeth - 1;
+                //Console.Out.WriteLine(hostping(host_a));
+                for (int i = 0; i < maxvett; i++)
+                {
+                    chartSeries1[i] = chartSeries1[i + 1];
+                }
+                chartSeries1[maxvett] = hostping(host_a);
+
+                scrivioutput_a(host_a + " - " + Convert.ToString(chartSeries1[maxvett]));
+                generagrafico1();
+                
+
             }
+
+            //ping 2
             if (!string.IsNullOrEmpty(host_b))
             {
-                Console.Out.WriteLine(hostping(host_b));
-                scrivioutput_b(host_b + " - " + Convert.ToString(hostping(host_b)));
+                int maxvett = pingDeth - 1;
+                //.Out.WriteLine(hostping(host_b));
+                for (int i = 0; i < maxvett; i++)
+                {
+                    chartSeries2[i] = chartSeries2[i + 1];
+                }
+                chartSeries2[maxvett] = hostping(host_b);
+
+                scrivioutput_b(host_b + " - " + Convert.ToString(chartSeries2[maxvett]));
+                generagrafico2();
+
             }
+
+        }
+
+        private void generagrafico1()
+        {
+            //inizializzazione grafico1
+            chart1.Series.Clear();
+            var series1 = new System.Windows.Forms.DataVisualization.Charting.Series
+            {
+                Name = "ms",
+                Color = System.Drawing.Color.Blue,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column
+            };
+
+            chart1.Series.Add(series1);
+            for (int i = 0; i < pingDeth; i++)
+            {
+                // Add series.
+                series1.Points.AddXY(i, chartSeries1[i]);
+            }
+        }
+        private void generagrafico2()
+        {
+            //inizializzazione grafico2
+            chart2.Series.Clear();
+            var series1 = new System.Windows.Forms.DataVisualization.Charting.Series
+            {
+                Name = "ms",
+                Color = System.Drawing.Color.Red,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column
+            };
+
+            chart2.Series.Add(series1);
+            for (int i = 0; i < pingDeth; i++)
+            {
+                // Add series.
+                series1.Points.AddXY(i, chartSeries2[i]);
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
 
         }
     }
